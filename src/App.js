977bd26c24c25ffde05fbaa9e6f85ab9d7f28a11
Abood Asfari <abdz2004@gmail.com>
2023-07-04 20:59:54 +1200

    Finalized look of projects page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import { 
   AppBar, 
   Button, 
+  CssBaseline, 
   Stack, 
+  ThemeProvider, 
   Toolbar, 
 } from "@mui/material";
+import portfolioTheme from "./PortfolioTheme";
 import Home from "./Home";
 import Projects from "./Projects";
 import Skills from "./Skills";
@@ -19,20 +22,21 @@ const App = () => {
   const [activePage, setActivePage] = React.useState(MainPages.PROJECTS);
 
   return (
-    <>
-    <AppBar position="sticky" component="nav" sx={{ boxShadow: 0 }}>
-        <Toolbar>
-          <Stack spacing={1} direction={"row"} sx={{ justifyContent: "end", width: "100%" }}>
-            <Button variant="contained" onClick={() => setActivePage(MainPages.HOME)}> Home </Button> 
-            <Button variant="contained" onClick={() => setActivePage(MainPages.PROJECTS)}> Projects </Button> 
-            <Button variant="contained" onClick={() => setActivePage(MainPages.SKILLS)}> Skills </Button> 
-          </Stack>
-        </Toolbar>
-    </AppBar>
-    {activePage === MainPages.HOME && <Home />}
-    {activePage === MainPages.PROJECTS && <Projects />}
-    {activePage === MainPages.SKILLS && <Skills />}
-    </>
+    <ThemeProvider theme={portfolioTheme}>
+      <CssBaseline /> 
+      <AppBar position="sticky" component="nav" sx={{ boxShadow: 0 }}>
+          <Toolbar>
+            <Stack spacing={1} direction={"row"} sx={{ justifyContent: "end", width: "100%" }}>
+              <Button variant="text" disableRipple sx={{ fontSize: "18px", textTransform: "none", fontWeight: 700, "&:hover": { backgroundColor: "background.default", textDecoration: "underline", textDecorationThickness: "3px" } }} onClick={() => setActivePage(MainPages.HOME)}> Home </Button> 
+              <Button variant="text" disableRipple sx={{ fontSize: "18px", textTransform: "none", fontWeight: 700, "&:hover": { backgroundColor: "background.default", textDecoration: "underline", textDecorationThickness: "3px" } }} onClick={() => setActivePage(MainPages.PROJECTS)}> Projects </Button> 
+              <Button variant="text" disableRipple sx={{ fontSize: "18px", textTransform: "none", fontWeight: 700, "&:hover": { backgroundColor: "background.default", textDecoration: "underline", textDecorationThickness: "3px" } }} onClick={() => setActivePage(MainPages.SKILLS)}> Skills </Button> 
+            </Stack>
+          </Toolbar>
+      </AppBar>
+      {activePage === MainPages.HOME && <Home />}
+      {activePage === MainPages.PROJECTS && <Projects />}
+      {activePage === MainPages.SKILLS && <Skills />}
+    </ThemeProvider>
   );
 };
 
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -8,9 +8,14 @@ import {
   CardMedia, 
   Icon, 
   IconButton, 
+  Pagination, 
+  PaginationItem, 
   Stack, 
+  ThemeProvider, 
   Typography 
 } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LanguageIcon from '@mui/icons-material/Language';
 
@@ -22,22 +27,22 @@ const Projects = () => {
     <Stack sx={{ alignItems: "center", mt: "2ch", width: "100%" }}>
       <Stack direction="row" sx={{ justifyContent: "center" }}> 
         <Box sx={{ width: "9%" }} />
-        <Box component="img" src="2359preview.png" sx={{ width: "55%", height: "55%", borderRadius: "6px", border: "solid 2ch", borderColor: "red" }} /> 
+        <Box component="img" src="2359preview.png" sx={{ width: "55%", height: "55%", borderRadius: "6px", border: "solid 1.5ch", borderColor: "accent.main" }} /> 
         <Stack spacing={2} sx={{ mt: 2, width: "9%" }}>
           {hello.map((item, index) => (
             <Box sx={{ width: "100%", height: "10%", boxShadow: 0, borderRadius: "0 6px 6px 0", justifySelf: "left", 
-              display: "flex", backgroundColor: "red" }}>
-              <Typography sx={{ alignSelf: "center", ml: "1ch" }}> {item} </Typography>
+              display: "flex", backgroundColor: "accent.main" }}>
+              <Typography sx={{ alignSelf: "center", ml: "1ch", fontWeight: 600, color: "white" }}> {item} </Typography>
             </Box>
           ))}
 
           <Box>
-            <IconButton size="large" sx={{ backgroundColor: "red", ml: "2ch" }}>
+            <IconButton size="large" sx={{ backgroundColor: "#ced4da", ml: "2.5ch", "&:hover": { backgroundColor: "#dde4eb" } }}>
               <GitHubIcon />
             </IconButton>
           </Box>
           <Box>
-            <IconButton size="large" sx={{ backgroundColor: "red", ml: "2ch" }}>
+            <IconButton size="large" sx={{ backgroundColor: "#a5d8ff", ml: "2.5ch", "&:hover": { backgroundColor: "#c2dcf0" } }}>
               <LanguageIcon />
             </IconButton>
           </Box>
@@ -45,29 +50,33 @@ const Projects = () => {
       </Stack>
 
       <Stack direction="row" spacing={2} sx={{ mt: "2ch" }}>
-        <IconButton color="red" sx={{ backgroundColor: "purple"}}> </IconButton>
-        <IconButton color="red" sx={{ backgroundColor: "red"}}> </IconButton>
-        <IconButton color="red" sx={{ backgroundColor: "red"}}> </IconButton>
+        <IconButton color="red" sx={{ backgroundColor: "accent.secondary", "&:hover": { backgroundColor: "#c4b99f" } }}> </IconButton>
+        <IconButton color="red" sx={{ backgroundColor: "#A19882", "&:hover": { backgroundColor: "#c4b99f" } }}> </IconButton>
+        <IconButton color="red" sx={{ backgroundColor: "#A19882", "&:hover": { backgroundColor: "#c4b99f" } }}> </IconButton>
       </Stack>
-      <Stack direction="row" sx={{ mx: "15%", mt: "5ch" }}> 
-        <Stack>
-          <Typography>
-            This project is dear to me (probably why it's the the first one I'm showing). It was my introduction to web development 
-            and to my surprise, I loved it! After being introduced to React and AWS, I realized that web development was much less restrictive 
-            than I had initially imagined. Working on 23:59 also reignited my love for working with others. But what is 23:59? It's a 
-            web development grade tracking website with full account management, and a unique course template system where students 
-            can use templates created by other students, as well as create their own. Pretty cool.
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Stack direction="row" spacing={1} sx={{ backgroundColor: "red", width: "calc(80% + 3ch)", p: "1ch", alignSelf: "start", justifyContent: "center", borderRadius: "6px" }}>
-            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
-            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
-            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "blue" }} />
-            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
+      <Stack direction="row" sx={{ mx: "10%", mt: "5ch" }}> 
+        <Typography sx={{ fontWeight: 600, width: "40%", ml: "calc(10% - 3ch)" }}>
+          This project is dear to me (probably why it's the the first one I'm showing). It was my introduction to web development 
+          and to my surprise, I loved it! After being introduced to React and AWS, I realized that web development was much less restrictive 
+          than I had initially imagined. Working on 23:59 also reignited my love for working with others. But what is 23:59? It's a 
+          web development grade tracking website with full account management, and a unique course template system where students 
+          can use templates created by other students, as well as create their own. Pretty cool.
+        </Typography>
+        
+        <Stack spacing={2} sx={{ ml: "6ch", width: "40%", height: "40%", alignItems: "center" }}>
+          <Box component="img" src="2359example1.png" sx={{ width: "100%", height: "100%", borderRadius: "6px" }} /> 
+          <Stack spacing={2}>
+            <Pagination
+              count={3}
+              renderItem={(item) => (
+                <PaginationItem sx={{ color: "accent.secondary", fontWeight: 600 }} 
+                  slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+                  {...item}
+                />
+              )}
+            />
           </Stack>
         </Stack>
-
-        <Box component="img" src="2359example1.png" sx={{ ml: "2ch", width: "40%", height: "40%", borderRadius: "6px", border: "solid 1ch", borderColor: "red" }} /> 
       </Stack>
     </Stack>
     </>
