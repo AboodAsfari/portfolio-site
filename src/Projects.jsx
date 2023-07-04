96079339bfba8e8df388e1e5c3b3049f60b3a184
Abood Asfari <abdz2004@gmail.com>
2023-07-04 13:43:28 +1200

    Changed preview image carousel to %

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -22,13 +22,7 @@ const Projects = () => {
     <Stack sx={{ alignItems: "center", mt: "2ch", width: "100%" }}>
       <Stack direction="row" sx={{ justifyContent: "center" }}> 
         <Box sx={{ width: "9%" }} />
-        <Card sx={{ width: "55%", boxShadow: 0, backgroundColor: "red" }}>
-          <CardMedia sx={{ m: "2ch", width: "calc(100% - 4ch)" }}
-            component="img"
-            alt="project preview"
-            image="2359preview.png"
-          />
-        </Card>
+        <Box component="img" src="2359preview.png" sx={{ width: "55%", height: "55%", borderRadius: "6px", border: "solid 2ch", borderColor: "red" }} /> 
         <Stack spacing={2} sx={{ mt: 2, width: "9%" }}>
           {hello.map((item, index) => (
             <Box sx={{ width: "100%", height: "10%", boxShadow: 0, borderRadius: "0 6px 6px 0", justifySelf: "left", 
@@ -55,7 +49,7 @@ const Projects = () => {
         <IconButton color="red" sx={{ backgroundColor: "red"}}> </IconButton>
         <IconButton color="red" sx={{ backgroundColor: "red"}}> </IconButton>
       </Stack>
-      <Stack direction="row" sx={{ mx: "25ch", mt: "5ch" }}>
+      <Stack direction="row" sx={{ mx: "15%", mt: "5ch" }}> 
         <Stack>
           <Typography>
             This project is dear to me (probably why it's the the first one I'm showing). It was my introduction to web development 
@@ -65,15 +59,15 @@ const Projects = () => {
             can use templates created by other students, as well as create their own. Pretty cool.
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
-          <Stack direction="row" spacing={1} sx={{ backgroundColor: "red", width: "calc(800px + 3ch)", p: "1ch", alignSelf: "start", justifyContent: "center", borderRadius: "6px" }}>
-            <Box component="img" src="2359example1.png" sx={{ width: "200px", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
-            <Box component="img" src="2359example1.png" sx={{ width: "200px", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
-            <Box component="img" src="2359example1.png" sx={{ width: "200px", borderRadius: "6px", border: "solid 0px", borderColor: "blue" }} />
-            <Box component="img" src="2359example1.png" sx={{ width: "200px", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
+          <Stack direction="row" spacing={1} sx={{ backgroundColor: "red", width: "calc(80% + 3ch)", p: "1ch", alignSelf: "start", justifyContent: "center", borderRadius: "6px" }}>
+            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
+            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
+            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "blue" }} />
+            <Box component="img" src="2359example1.png" sx={{ width: "24%", borderRadius: "6px", border: "solid 0px", borderColor: "red" }} />
           </Stack>
         </Stack>
 
-        <Box component="img" src="2359example1.png" sx={{ ml: "2ch", width: "630px", borderRadius: "6px", border: "solid 1ch", borderColor: "red" }} /> 
+        <Box component="img" src="2359example1.png" sx={{ ml: "2ch", width: "40%", height: "40%", borderRadius: "6px", border: "solid 1ch", borderColor: "red" }} /> 
       </Stack>
     </Stack>
     </>
