e9292b77ad19919d9a1c572ae3b251dba8b9ed01
Abood Asfari <abdz2004@gmail.com>
2023-07-06 23:14:23 +1200

    Wrote content for home page and implemented basic name animation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Box, 
   Button, 
   CssBaseline, 
+  Fade, 
   Stack, 
   ThemeProvider, 
   Toolbar,
@@ -13,6 +14,7 @@ import portfolioTheme from "./PortfolioTheme";
 import Home from "./Home";
 import Projects from "./Projects";
 import Skills from "./Skills";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 const App = () => {
   const MainPages = {
@@ -21,24 +23,69 @@ const App = () => {
     SKILLS: 2
   }
 
-  const [activePage, setActivePage] = React.useState(MainPages.PROJECTS);
+  const [activePage, setActivePage] = React.useState(MainPages.HOME);
+  const [nameSize, setNameSize] = React.useState(104);
+  const [nameTopOffset, setNameTopOffset] = React.useState(30);
+  const nameGrow = React.useRef(true);
+  const queuedTransition = React.useRef(null);
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setNameSize(oldSize => { 
+        if (oldSize === 60 && queuedTransition.current !== null) {
+          queuedTransition.current();
+          queuedTransition.current = null;
+        }
+        
+        if (!nameGrow.current && oldSize > 24) return oldSize - 2;
+        else if (nameGrow.current && oldSize < 104) return oldSize + 2; 
+        else return oldSize; 
+      });
+
+      setNameTopOffset(oldOffset => { 
+        if (!nameGrow.current && oldOffset > 10) return oldOffset - 0.5;
+        else if (nameGrow.current && oldOffset < 30) return oldOffset + 0.5;
+        else return oldOffset; 
+      });
+    }, 10);
+  }, []);
+
+  const openProjects = () => {
+    if (activePage !== MainPages.HOME) setActivePage(MainPages.PROJECTS);
+    else {
+      queuedTransition.current = () => setActivePage(MainPages.PROJECTS); 
+      nameGrow.current = false;
+    }
+  };
+
+  const openHome = () => {
+    if (activePage === MainPages.HOME) return;
+    else {
+      setActivePage(MainPages.HOME); 
+      nameGrow.current = true;
+    }
+  };
 
   return (
     <ThemeProvider theme={portfolioTheme}>
       <CssBaseline /> 
+      <Typography sx={{ fontSize: nameSize + "px", fontWeight: 700, color: "text.main", position: "absolute", top: nameTopOffset + "px", left: "0.5%", zIndex: 99999 }}> Abdulrahman Asfari </Typography>
       <AppBar position="sticky" component="nav" sx={{ boxShadow: 0 }}>
           <Toolbar sx={{ display: "flex" }}>
-            <Typography sx={{ fontSize: "24px", fontWeight: 700, color: "primary.main" }}> Abdulrahman Asfari </Typography>
             <Box sx={{ flexGrow: 1 }} />
             <Stack spacing={1} direction={"row"} sx={{ justifyContent: "end" }}>
-              <NavButton action={() => setActivePage(MainPages.HOME)} label="Home" />
-              <NavButton action={() => setActivePage(MainPages.PROJECTS)} label="Projects" />
+              <NavButton action={openHome} label="Home" />
+              <NavButton action={openProjects} label="Projects" />
               <NavButton action={() => setActivePage(MainPages.SKILLS)} label="Skills" />
             </Stack>
           </Toolbar>
       </AppBar>
       {activePage === MainPages.HOME && <Home />}
-      {activePage === MainPages.PROJECTS && <Projects />}
+      <Fade in={activePage === MainPages.PROJECTS}>
+        <Box>
+          { <Projects /> }
+        </Box>
+      </Fade>
       {activePage === MainPages.SKILLS && <Skills />}
     </ThemeProvider>
   );
@@ -51,7 +98,7 @@ const NavButton = (props) => {
   } = props;
 
   return (
-    <Button variant="text" disableRipple sx={{ fontSize: "18px", textTransform: "none", fontWeight: 700, 
+    <Button variant="text" disableRipple sx={{ fontSize: "18px", color: "text.main", textTransform: "none", fontWeight: 700, 
       "&:hover": { backgroundColor: "background.default", textDecoration: "underline", textDecorationThickness: "3px" } }} 
       onClick={action}> {label} </Button> 
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,10 +6,23 @@ import {
 
 const Home = () => {
   return (
-    <Stack>
-      <Typography> NAME </Typography>
-      <Typography> NICKNAME </Typography>
-      <Typography> ABOUT ME! </Typography>
+    <Stack sx={{ mt: "10ch", ml: "0.5%  " }}>
+      <Typography sx={{ fontSize: "30px", fontWeight: 700, color: "text.secondary" }}> Or Abood to keep it simple :) </Typography>
+      <Typography sx={{ fontSize: "20px", fontWeight: 700, color: "text.accent", mt: "4ch", width: "60%" }}> 
+        Hi there, I'm Abood! Welcome to my portfolio. Here I show off some of my projects and skills, while maintaining a 
+        cozy atmosphere across the whole page. I'm a software engineering student in my penultimate year of study. In my degree
+        I am specializing in embedded systems as well as networked applications, although you wouldn't guess it by the projects 
+        I work on outside of university. 
+      </Typography>
+      <Typography sx={{ fontSize: "20px", fontWeight: 700, color: "text.accent", mt: "2ch", width: "60%" }}> 
+        This year I unexpectedly fell in love with building web applications and full stack development as a whole, after almost 5 
+        years of despising it with a passion. How this radical change came to be is still beyond me, but I accredit at least some of 
+        it to me discovering React and how intuitive it makes the whole process for me. 
+      </Typography>
+      <Typography sx={{ fontSize: "20px", fontWeight: 700, color: "text.accent", mt: "2ch", width: "60%" }}> 
+        Anyways, at this point I'm just rambling. Feel free to take a look around the website, or even contact me if you wish,
+        using the handy little device I've left on your screen. Either way, enjoy your stay here!
+      </Typography>
     </Stack>
   );
 };
