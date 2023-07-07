5fe3fd38b8b1b61f9b71c39e077eaef3ccf8c38f
Abood Asfari <abdz2004@gmail.com>
2023-07-07 19:44:00 +1200

    Contact device now slides

--- src/Home.jsx
+++ src/Home.jsx
@@ -41,32 +41,34 @@ const Home = (props) => {
         </Slide>
       </Stack>
       <Box sx={{ width: "calc(45%)", height: "100%", display: "flex", justifyContent: "center" }}>
-        <Stack sx={{ backgroundColor: "text.secondary", width: "360px", height: "592px", mt: "21ch", borderRadius: "6px 6px 60px 6px" }}>
-          <Box sx={{ backgroundColor: "#606C5D", width: "90%", height: "40%", mx: "5%", mt: "10%", borderRadius: "6px 6px 30px 6px" }}> 
-            <Box sx={{ position: "relative", backgroundColor: "#B5C99A", width: "70%", height: "80%", mx: "15%", mt: "7%" }}>
-              <Box class="scanline" />
-              <Box id={"crt"} sx={{ width: "100%", height: "100%" }}> 
-                <Box sx={{ display: "flex", justifyContent: "center" }}>
-                  <img alt="POOP" src="githublogo.png" width="60%" height="auto" style={{ marginTop: "12%"}} />
+        <Slide in={showContent} direction="left" timeout={{ enter: 500, exit: 200 }}>
+          <Stack sx={{ backgroundColor: "text.secondary", width: "360px", height: "592px", mt: "21ch", borderRadius: "6px 6px 60px 6px" }}>
+            <Box sx={{ backgroundColor: "#606C5D", width: "90%", height: "40%", mx: "5%", mt: "10%", borderRadius: "6px 6px 30px 6px" }}> 
+              <Box sx={{ position: "relative", backgroundColor: "#B5C99A", width: "70%", height: "80%", mx: "15%", mt: "7%" }}>
+                <Box class="scanline" />
+                <Box id={"crt"} sx={{ width: "100%", height: "100%" }}> 
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <img alt="POOP" src="githublogo.png" width="60%" height="auto" style={{ marginTop: "12%"}} />
+                  </Box>
+                  <Typography sx={{ ml: "0.7ch", color: "#00000095", fontWeight: 600, mt: "0.3ch" }}> A - Open &nbsp; &nbsp; &nbsp;&nbsp; B - Also Open </Typography>
                 </Box>
-                <Typography sx={{ ml: "0.7ch", color: "#00000095", fontWeight: 600, mt: "0.3ch" }}> A - Open &nbsp; &nbsp; &nbsp;&nbsp; B - Also Open </Typography>
               </Box>
             </Box>
-          </Box>
-          <Stack direction="row" sx={{ mt: "10ch", ml: "10%" }}>
-            <Stack sx={{ alignItems: "center" }}>
-              <Box sx={{ backgroundColor: "text.accent", width: "25px", height: "33px", borderRadius: "3px 3px 0 0", cursor: "pointer", "&:active": { background: "linear-gradient(0deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
-              <Stack direction="row">
-                <Box sx={{ backgroundColor: "text.accent", width: "33px", height: "25px", borderRadius: "3px 0 0 3px", cursor: "pointer", "&:active": { background: "linear-gradient(-90deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
-                <Box sx={{ backgroundColor: "text.accent", width: "25px", height: "25px" }} />
-                <Box sx={{ backgroundColor: "text.accent", width: "33px", height: "25px", borderRadius: "0 3px 3px 0", cursor: "pointer", "&:active": { background: "linear-gradient(90deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
+            <Stack direction="row" sx={{ mt: "10ch", ml: "10%" }}>
+              <Stack sx={{ alignItems: "center" }}>
+                <Box sx={{ backgroundColor: "text.accent", width: "25px", height: "33px", borderRadius: "3px 3px 0 0", cursor: "pointer", "&:active": { background: "linear-gradient(0deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
+                <Stack direction="row">
+                  <Box sx={{ backgroundColor: "text.accent", width: "33px", height: "25px", borderRadius: "3px 0 0 3px", cursor: "pointer", "&:active": { background: "linear-gradient(-90deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
+                  <Box sx={{ backgroundColor: "text.accent", width: "25px", height: "25px" }} />
+                  <Box sx={{ backgroundColor: "text.accent", width: "33px", height: "25px", borderRadius: "0 3px 3px 0", cursor: "pointer", "&:active": { background: "linear-gradient(90deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
+                </Stack>
+                <Box sx={{ backgroundColor: "text.accent", width: "25px", height: "33px", borderRadius: "0 0 3px 3px", cursor: "pointer", "&:active": { background: "linear-gradient(180deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
               </Stack>
-              <Box sx={{ backgroundColor: "text.accent", width: "25px", height: "33px", borderRadius: "0 0 3px 3px", cursor: "pointer", "&:active": { background: "linear-gradient(180deg, rgba(103,93,80,1) 10%, rgba(77,69,60,1) 80%);" } }} />
+              <Box sx={{ borderRadius: "50%", backgroundColor: "#F1C376", width: "50px", height: "50px", alignSelf: "center", ml: "80px", mt: "45px", cursor: "pointer", "&:active": { background: "radial-gradient(circle, rgba(241,195,118,1) 50%, rgba(163,124,60,1) 100%);" } }}></Box>
+              <Box sx={{ borderRadius: "50%", backgroundColor: "#F1C376", width: "50px", height: "50px", alignSelf: "center", ml: "20px", mb: "20px", cursor: "pointer", "&:active": { background: "radial-gradient(circle, rgba(241,195,118,1) 50%, rgba(163,124,60,1) 100%);" } }}></Box>
             </Stack>
-            <Box sx={{ borderRadius: "50%", backgroundColor: "#F1C376", width: "50px", height: "50px", alignSelf: "center", ml: "80px", mt: "45px", cursor: "pointer", "&:active": { background: "radial-gradient(circle, rgba(241,195,118,1) 50%, rgba(163,124,60,1) 100%);" } }}></Box>
-            <Box sx={{ borderRadius: "50%", backgroundColor: "#F1C376", width: "50px", height: "50px", alignSelf: "center", ml: "20px", mb: "20px", cursor: "pointer", "&:active": { background: "radial-gradient(circle, rgba(241,195,118,1) 50%, rgba(163,124,60,1) 100%);" } }}></Box>
           </Stack>
-        </Stack>
+        </Slide>
       </Box>
     </Stack>
   );
